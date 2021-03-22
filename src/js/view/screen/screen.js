6a0698e863f33ffa5afffc2ee0efab245557fca6
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2021-03-22 14:58:46 -0600

    add copyright header to new file

--- src/js/view/screen/screen.js
+++ src/js/view/screen/screen.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2021 Moneysocket Developers
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php
 const D = require('../../utl/dom.js').DomUtl;
 const I = require('../../utl/icon.js').IconUtl;
 
